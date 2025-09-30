14b254e795991823d40b58bdbb9829373eef4a81
BenjaUrbin <benjamin.urbina@userena.cl>
2025-09-30 19:57:13 -0300

    Muchos cambios, perdon :C

---
+++ Lab3-AppInt/src/assets/Home - NOTeolvides_files/main.tsx
@@ -0,0 +1,15 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=1befca67"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=c2f1f58f"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import { BrowserRouter } from "/node_modules/.vite/deps/react-router-dom.js?v=6e0fab4a";
+import App from "/src/App.tsx?t=1759263989171";
+createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(BrowserRouter, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/Users/benjamin.urbina/Desktop/Archivos Generales/Programacion/Proyectos/AplicacionesInt/Lab3/Lab3-AppInt/src/main.tsx",
+  lineNumber: 5,
+  columnNumber: 5
+}, this) }, void 0, false, {
+  fileName: "/Users/benjamin.urbina/Desktop/Archivos Generales/Programacion/Proyectos/AplicacionesInt/Lab3/Lab3-AppInt/src/main.tsx",
+  lineNumber: 4,
+  columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBTUk7QUFOSixTQUFTQSxrQkFBa0I7QUFDM0IsU0FBU0MscUJBQXFCO0FBQzlCLE9BQU9DLFNBQVM7QUFFaEJGLFdBQVdHLFNBQVNDLGVBQWUsTUFBTSxDQUFFLEVBQUVDLE9BQzNDLHVCQUFDLGlCQUNDLGlDQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUFJLEtBRE47QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUVBLENBQ0YiLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQnJvd3NlclJvdXRlciIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgeyBCcm93c2VyUm91dGVyIH0gZnJvbSBcInJlYWN0LXJvdXRlci1kb21cIjtcbmltcG9ydCBBcHAgZnJvbSBcIi4vQXBwLnRzeFwiO1xuXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSEpLnJlbmRlcihcbiAgPEJyb3dzZXJSb3V0ZXI+XG4gICAgPEFwcCAvPlxuICA8L0Jyb3dzZXJSb3V0ZXI+XG4pO1xuIl0sImZpbGUiOiIvVXNlcnMvYmVuamFtaW4udXJiaW5hL0Rlc2t0b3AvQXJjaGl2b3MgR2VuZXJhbGVzL1Byb2dyYW1hY2lvbi9Qcm95ZWN0b3MvQXBsaWNhY2lvbmVzSW50L0xhYjMvTGFiMy1BcHBJbnQvc3JjL21haW4udHN4In0=
